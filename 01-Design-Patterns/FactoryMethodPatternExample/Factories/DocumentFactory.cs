b63d46bffddd072d044ea852d7ccf90fa0684237
udaykumar18107-hub <udaykumar18107@gmail.com>
2026-07-09 19:09:43 +0530

    Delete all project files

--- 01-Design-Patterns/FactoryMethodPatternExample/Factories/DocumentFactory.cs
+++
@@ -1,9 +0,0 @@
-using FactoryMethodPatternExample;
-
-namespace FactoryMethodPatternExample
-{
-    public abstract class DocumentFactory
-    {
-        public abstract IDocument CreateDocument();
-    }
-}
